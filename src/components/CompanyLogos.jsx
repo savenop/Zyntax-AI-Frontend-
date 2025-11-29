74cb5ff88bda324b034b7ed7272c9878c4d18ab2
devdrift7 <devanshchaudhary.work@gmail.com>
2025-11-29 14:40:49 +0530

    First Commit

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -3,7 +3,7 @@ import { companyLogos } from "../constants";
 const CompanyLogos = ({ className }) => {
   return (
     <div className={className}>
-      <h5 className="tagline mb-6 text-center text-n-1/50">
+      {/* <h5 className="tagline mb-6 text-center text-n-1/50">
         Helping people create beautiful content at
       </h5>
       <ul className="flex">
@@ -13,9 +13,10 @@ const CompanyLogos = ({ className }) => {
             key={index}
           >
             <img src={logo} width={134} height={28} alt={logo} />
+            <img src="brainwave\src\assets\Zyntax logo.png" alt="" />
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
-import { brainwave } from "../assets";
+import { Zyntax } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
@@ -35,9 +35,9 @@ const Header = () => {
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+      <div className="flex items-center h-20 px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="Brainwave" />
+          <img src={Zyntax} width={190} height={40} alt="Zyntax" />
         </a>
 
         <nav
@@ -74,7 +74,7 @@ const Header = () => {
           New account
         </a>
         <Button className="hidden lg:flex" href="#login">
-          Sign in
+          Sign UP
         </Button>
 
         <Button
